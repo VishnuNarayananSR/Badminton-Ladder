e27a12849a132a047933fd2dc6295d14e5a12453
Shishere007 <55714591+Shishere007@users.noreply.github.com>
2021-04-13 17:52:00 +0530

    more file.py functions

--- File.py
+++ File.py
@@ -26,30 +26,30 @@ class File:
                 challenges.append(item)
         return challenges
 
-    def get_latest_n_challenges(self, cnt:int) -> list:
+    def get_latest_n_challenges(self, cnt: int) -> list:
         datas = reversed(self.read_data_file())
         challenges = [item for item in datas if len(item.split("/")) == 4]
         challenge_cnt = len(challenges)
         if cnt > challenge_cnt:
             cnt = challenge_cnt
         return challenges[:cnt]
-    
-    def update_score(self,challenge_data:str,score:str)->None:
+
+    def update_score(self, challenge_data: str, score: str) -> None:
         '''
         arguments sample:
         challenge_data:str  =   name 1 1/name 2 2/2020-12-12/None
         score:str           =   12-24 15-18 
         '''
         datas = self.read_data_file()
-        for ind,item in enumerate(datas):
+        for ind, item in enumerate(datas):
             if item.__contains__(challenge_data):
-                datas[ind] = item.replace('None',score)
+                datas[ind] = item.replace('None', score)
                 break
-        with open(file=self.data_filename,mode='w',encoding='utf-8') as file:
+        with open(file=self.data_filename, mode='w', encoding='utf-8') as file:
             for item in datas:
                 file.write(f"{item}\n")
 
-    def rewrite_ladder_order(self, player_list:list) -> None:
+    def rewrite_ladder_order(self, player_list: list) -> None:
         with open(file=self.ladder_filename, mode='w', encoding='utf-8') as file:
             for player in player_list:
                 file.write(f'{player}\n')
@@ -63,12 +63,83 @@ class File:
     def remove_player(self, player_name: str, _date: str) -> None:
         open(file=self.data_filename, mode='a',
              encoding='utf-8').write(f'-EX/{player_name}/{_date}\n')
-    
-    def player_already_present(self,player_name:str)->bool():
+
+    def player_already_present(self, player_name: str) -> bool():
         player_list = self.get_ladder_player_list()
         if player_name in player_list:
             return True
         return False
+
+    def get_challenges_by_player_name(self,name1: str, name2: str) -> list:
+        datas = self.read_data_file()
+        ret_data = []
+        for item in datas:
+            temp = item.split("/")
+            if len(temp) != 4:
+                continue
+            if item.__contains__(name1) and item.__contains__(name2):
+                ret_data.append(item)
+        return ret_data
+
+    def get_challenges_by_date(self,date: str) -> list:
+        datas = self.read_data_file()
+        ret_data = []
+        for item in datas:
+            temp = item.split("/")
+            if len(temp) != 4:
+                continue
+            if item.__contains__(date):
+                ret_data.append(item)
+        return ret_data
+
+    def get_most_active_player(self) -> str:
+        player_list = self.get_ladder_player_list()
+        datas = self.read_data_file()
+        match_data = {}
+        for player in player_list:
+            for item in datas:
+                temp = item.split("/")
+                if len(temp) != 4:
+                    continue
+                player_1_name = " ".join(temp[0].split()[:-1])
+                player_2_name = " ".join(temp[1].split()[:-1])
+                if player in [player_1_name,player_2_name]:
+                    if player in match_data.keys():
+                        match_data[player] += 1
+                    else:
+                        match_data[player] = 1
+            else:
+                if player not in match_data.keys():
+                    match_data[player] = 0
+        high_cnt = max(match_data.values())
+        print(match_data)
+        for key, value in match_data.items():
+            if value == high_cnt:
+                return key
+
+    def get_least_active_player(self) -> str:
+        player_list = self.get_ladder_player_list()
+        datas = self.read_data_file()
+        match_data = {}
+        for player in player_list:
+            for item in datas:
+                temp = item.split("/")
+                if len(temp) != 4:
+                    continue
+                player_1_name = " ".join(temp[0].split()[:-1])
+                player_2_name = " ".join(temp[1].split()[:-1])
+                if player in [player_1_name,player_2_name]:
+                    if player in match_data.keys():
+                        match_data[player] += 1
+                    else:
+                        match_data[player] = 1
+                    break
+            else:
+                match_data[player] = 0
+        high_cnt = min(match_data.values())
+        for key, value in match_data.items():
+            if value == high_cnt:
+                return key
 
 
 if __name__ == "__main__":
