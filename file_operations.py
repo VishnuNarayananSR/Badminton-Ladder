f996882125e0b85834dca5509ec092062555a221
Shishere007 <55714591+Shishere007@users.noreply.github.com>
2021-04-13 13:12:54 +0530

    file operations

--- file_operations.py
+++ file_operations.py
@@ -10,3 +10,13 @@ def read_data_file():
 def get_ladder_player_list():
     return open(file=ladder_filename,mode='r',encoding='utf-8').readlines()
 
+def add_player(player_name):
+    open(file=ladder_filename,mode='a',encoding='utf-8').write(f'{data_to_write}\n')
+
+
+def remove_player(player_name):
+    player_list = open(file=ladder_filename,mode='r',encoding='utf-8').readlines()
+    player_list.remove(player_name)
+    with open(file=data_filename,mode='w',encoding='utf-8') as file:
+        for player in player_list:
+            file.writ(f"{player}\n")
